f096f1c94c5def33dffc1e238ed17fb85230573d
Spadilla1018 <spadillar@ucundinamarca.edu.co>
2025-10-25 13:17:41 -0500

    Cambios en  templates de base, inicio, nosotros y producto
    Se realizaron cambios en las plantillas HTML y en el archivo app.py para mejorar la funcionalidad y el diseño del sitio web.

--- app.py
+++ app.py
@@ -1570,4 +1570,4 @@ def nosotros():
 # Run
 # ----------------------------------
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000)                                
